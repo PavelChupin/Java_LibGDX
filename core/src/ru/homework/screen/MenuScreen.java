0ad621378c9d32af1904e5c9bb6e4ce6be63601c
Chupin Pevel <pavel.chupin@gmail.com>
2019-12-17 15:53:15 +0700

    ДЗ2

--- core/src/ru/homework/screen/MenuScreen.java
+++ core/src/ru/homework/screen/MenuScreen.java
@@ -1,17 +1,18 @@
 package ru.homework.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import ru.homework.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
-
+    private static final float DIST = 10f;
     private Texture img;
     private Texture backGround;
     private Vector2 pos;
+    private Vector2 posTo;
     private Vector2 speed;
 
     @Override
@@ -21,7 +22,8 @@ public class MenuScreen extends BaseScreen {
         img = new Texture("badlogic.jpg");
         backGround = new Texture("textures/starBackGround.jpg");
         pos = new Vector2();
-        speed = new Vector2(0.5f,0.5f);
+        posTo = new Vector2();
+        speed = new Vector2();
     }
 
     @Override
@@ -32,14 +34,27 @@ public class MenuScreen extends BaseScreen {
         batch.begin();
         //Отрисовываем фон
         //batch.setColor(1,1,1,1f);
-        batch.draw(backGround, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        batch.draw(backGround, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         //batch.setColor(0.3f,0.5f,0.6f,1f);
         batch.draw(img, pos.x, pos.y);
         //batch.draw(region,300,300);
         batch.end();
 
-        pos.add(speed);
+        actionObject(this.pos);
+    }
+
+    private void actionObject(Vector2 obj) {
+        if ((speed.x < 0 && obj.x > posTo.x)
+                || (speed.x > 0 && obj.x < posTo.x)
+                || (speed.y < 0 && obj.y > posTo.y)
+                || (speed.y > 0 && obj.y < posTo.y)
+        ) {
+            obj.add(speed);
+        }
+        //    if (/*!pos.equals(posTo) ||*/ (pos.x < posTo.x && pos.y < posTo.y)) {
+        //   pos.add(speed);
+        //}
     }
 
     @Override
@@ -52,7 +67,56 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos.set(screenX,Gdx.graphics.getHeight() - screenY);
+        System.out.println("touchDown screenX = " + screenX + " screenY " + screenY + " pointer " + pointer + " button " + button);
+        //Задаем точку до которой требуется двигаться
+        posTo.set(screenX, Gdx.graphics.getHeight() - screenY);
+        //Рассчитываем вектор скорости
+        calcSpeed();
+        //pos.set(screenX,Gdx.graphics.getHeight() - screenY);
         return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("keyDown keycode = " + keycode);
+        switch (keycode) {
+            case Input.Keys.DOWN: {
+                posTo.y -= DIST;
+                calcSpeed();
+                break;
+            }
+            case Input.Keys.UP: {
+                posTo.y += DIST;
+                calcSpeed();
+                break;
+            }
+            case Input.Keys.LEFT: {
+                posTo.x -= DIST;
+                calcSpeed();
+                break;
+            }
+            case Input.Keys.RIGHT: {
+                posTo.x += DIST;
+                calcSpeed();
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+        return false;//super.keyDown(keycode);
+    }
+
+    private void calcSpeed() {
+        Vector2 v = new Vector2(1, 1);
+        if (posTo.x < pos.x) {
+            v.x = -1;
+        }
+        if (posTo.y < pos.y) {
+            v.y = -1;
+        }
+        speed.set(posTo).nor().scl(v);
+        //System.out.println(speed);
     }
 }
