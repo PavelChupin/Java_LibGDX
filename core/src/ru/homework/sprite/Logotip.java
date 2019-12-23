1489badf24d1fbe1810db68b75012ae6f4fcc151
Chupin Pevel <pavel.chupin@gmail.com>
2019-12-23 16:53:29 +0700

    ДЗ3 начало задания. Финальная реализация.

--- core/src/ru/homework/sprite/Logotip.java
+++ core/src/ru/homework/sprite/Logotip.java
@@ -1,6 +1,5 @@
 package ru.homework.sprite;
 
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -9,39 +8,40 @@ import ru.homework.base.Sprite;
 import ru.homework.math.Rect;
 
 public class Logotip extends Sprite {
-    private static final float DIST = 0.01f;
+    private static final float SPEED = 0.01f;
+    private static final float OBJECT_SIZE_PROPORC = 0.25f;
 
     //Смещение при передвижении на клавиатуре
-    private float dist = DIST;
+    private float speed = SPEED;
 
     //Текущая позиция
     private Vector2 positionObj;
     //Позиция перемещения
     private Vector2 posTo;
     //Скорость перемещения
-    private Vector2 speed;
+    private Vector2 speedV;
 
 
     public Logotip(TextureRegion region) {
         super(region);
         this.positionObj = new Vector2();
         this.posTo = new Vector2();
-        this.speed = new Vector2();
+        this.speedV = new Vector2();
     }
 
-    public Logotip(TextureRegion region, float dist) {
+    public Logotip(TextureRegion region, float speed) {
         this(region);
-        this.dist = dist;
+        this.speed = speed;
     }
 
     private void changePosToKeyDown(float x, float y) {
-        this.posTo.add(this.dist * x, this.dist * y);
+        this.posTo.add(this.speed * x, this.speed * y);
         //Рассчитаем новую скорость объекта
         calcSpeed();
     }
 
     private void changePosToKeyDown(Vector2 vectorTo) {
-        this.posTo.add(vectorTo.cpy().scl(dist));
+        this.posTo.add(vectorTo.cpy().scl(speed));
         //Рассчитаем новую скорость объекта
         calcSpeed();
     }
@@ -54,17 +54,17 @@ public class Logotip extends Sprite {
 
     private void calcSpeed() {
         //Переделываем реализацию через setLenght, т.к. через nor() с мелкой сеткой float значений есть косяк.
-        this.speed.set(posTo).sub(positionObj).setLength(dist);
+        this.speedV.set(posTo).sub(positionObj).setLength(speed);
     }
 
     @Override
     public void actionObject() {
-        if ((speed.x < 0 && positionObj.x > posTo.x)
-                || (speed.x > 0 && positionObj.x < posTo.x)
-                || (speed.y < 0 && positionObj.y > posTo.y)
-                || (speed.y > 0 && positionObj.y < posTo.y)
+        if ((speedV.x < 0 && positionObj.x > posTo.x)
+                || (speedV.x > 0 && positionObj.x < posTo.x)
+                || (speedV.y < 0 && positionObj.y > posTo.y)
+                || (speedV.y > 0 && positionObj.y < posTo.y)
         ) {
-            this.positionObj.add(speed);
+            this.positionObj.add(speedV);
         }
     }
 
@@ -76,7 +76,7 @@ public class Logotip extends Sprite {
 
     @Override
     public void draw(SpriteBatch batch) {
-        batch.draw(regions[frame], positionObj.x, positionObj.y, halfWidth, halfHeight);
+        batch.draw(regions[frame], positionObj.x, positionObj.y, halfWidth*OBJECT_SIZE_PROPORC, halfHeight*OBJECT_SIZE_PROPORC);
     }
 
     @Override
@@ -105,11 +105,11 @@ public class Logotip extends Sprite {
         return false;
     }
 
-    public float getDist() {
-        return dist;
+    public float getSpeed() {
+        return speed;
     }
 
-    public void setDist(float dist) {
-        this.dist = dist;
+    public void setSpeed(float speed) {
+        this.speed = speed;
     }
 }
